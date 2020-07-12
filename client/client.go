c121d87d7a0d193b2cbf9396ea8f72c993942176
saurabh <saurabhgupta190699@gmail.com>
2020-07-12 14:01:58 +0530

    able to return array with serializing data

---
+++ client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"../chat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Error")
+	}
+
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	message := chat.Message{
+		Body: "Hello from the client!",
+	}
+
+	response, err := c.SayHello(context.Background(), &message)
+
+	log.Printf("Response from the Server: %s", response.Messages)
+}
